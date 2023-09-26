f630f6783abb866c1069cc1f9a6f200ea0a25e08
Redeller <login030@mail.ru>
2023-09-26 22:33:10 +0300

    added Exceptions

--- src/com/company/Logic.java
+++ src/com/company/Logic.java
@@ -1,11 +1,16 @@
 package com.company;
 
 public class Logic {
-    public String calculate(String primer) {
+    public String calculate(String primer) throws Exception {
         String[] arrayOfElements = primer.split(" ");
         int a = 0;
         int b = 0;
         int count = 0;
+        if (arrayOfElements.length == 1){
+            throw new Exception("строка не является математической операцией");
+        }else if (arrayOfElements.length > 3){
+            throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
         try {
             a = Integer.parseInt(arrayOfElements[0]);
         }catch (NumberFormatException e){
@@ -18,14 +23,22 @@ public class Logic {
             b = NumberConverter.romanToInt(arrayOfElements[2]);
             count++;
         }
+
         int result = signs(arrayOfElements[1],a,b);
+        if(result < 0){
+            throw new Exception("в римской системе нет отрицательных чисел");
+        }
+
         if (count==2){
             return NumberConverter.intToRoman(result);
-        }else {
+
+        }else if(count == 0){
         return String.valueOf(result);
         }
+
+        throw new Exception("используются одновременно разные системы счисления");
     }
-    private int signs(String znak, int a, int b) {
+    private int signs(String znak, int a, int b) throws Exception {
         int result = 0;
         switch (znak) {
             case "*":
@@ -40,7 +53,9 @@ public class Logic {
             case "-":
                 result = a - b;
                 break;
+            default: throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
+
         return result;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String calculation = sc.nextLine();
         Logic l = new Logic();
